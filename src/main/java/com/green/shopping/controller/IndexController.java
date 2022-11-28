f81520dadad2d8e4fc3275cf17ccd551265c4b61
JangJinseong <gaden7@naver.com>
2022-11-28 17:04:43 +0900

    초기설정파일 ㅇㅇ

--- src/main/java/com/green/shopping/controller/IndexController.java
+++ src/main/java/com/green/shopping/controller/IndexController.java
@@ -1,15 +1,25 @@
 package com.green.shopping.controller;
 
+import com.green.shopping.service.Service;
 import com.green.shopping.vo.SignUp;
+import com.green.shopping.vo.UserVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController("/api")
 public class IndexController {
 
+    @Autowired
+    Service service;
+
+
     @GetMapping("/name")
-    public String index() {
-        return "배민재, baeminjae";
+    public List<UserVo> index() {
+    List<UserVo> a = service.getList();
+        return a;
     }
 
     @PostMapping("/signup")
---
+++ src/main/java/com/green/shopping/service/Service.java
@@ -0,0 +1,10 @@
+package com.green.shopping.service;
+
+import com.green.shopping.vo.UserVo;
+
+import java.util.List;
+
+public interface Service  {
+
+    List<UserVo> getList();
+}
