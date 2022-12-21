49226d123a822cb01ff54ff60f65f46664123f8e
JangJinseong <gaden7@naver.com>
2022-12-21 18:27:04 +0900

    회원정보 수정 기능 추가

--- src/main/java/com/green/shopping/dao/UserDao.java
+++ src/main/java/com/green/shopping/dao/UserDao.java
@@ -1,10 +1,14 @@
 package com.green.shopping.dao;
 
 import java.util.HashMap;
+import java.util.Map;
+
 import com.green.shopping.vo.UserVo;
 
 public interface UserDao {
 
 
     UserVo finduser_information(String user_id);
+
+    void update_userinformation(Map<String, Object> map1);
 }
